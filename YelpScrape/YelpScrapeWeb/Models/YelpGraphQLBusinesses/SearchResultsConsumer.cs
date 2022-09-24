62d14fe7156b1b380f064b92e5ab11f16bfced0b
Benjamin Liu <benjaminliu221@gmail.com>
2022-09-24 16:03:30 -0700

    Added more arguments to searchbusiness query and display more fields on client response

--- YelpScrape/YelpScrapeWeb/Models/YelpGraphQLBusinesses/SearchResultsConsumer.cs
+++ YelpScrape/YelpScrapeWeb/Models/YelpGraphQLBusinesses/SearchResultsConsumer.cs
@@ -2,17 +2,17 @@
 {
     public interface ISearchResultsConsumer
     {
-        public SearchResults CreateSearchResults(SearchLocation searchLocation, List<Business> businesses);
+        public SearchResults CreateSearchResults(SearchArguments searchArguments, List<Business> businesses);
     }
     public class SearchResultsConsumer : ISearchResultsConsumer
     {
 
-        public SearchResults CreateSearchResults(SearchLocation searchLocation, List<Business> businesses)
+        public SearchResults CreateSearchResults(SearchArguments searchArguments, List<Business> businesses)
         {
             SearchResults searchResultsObj = new SearchResults()
             {
                 Businesses = businesses,
-                SearchLocation = searchLocation
+                SearchArguments = searchArguments
             };
             return searchResultsObj;
         }
