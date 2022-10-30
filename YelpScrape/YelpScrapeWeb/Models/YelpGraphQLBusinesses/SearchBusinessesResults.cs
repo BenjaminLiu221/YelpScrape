3801ffec138bb5a2b6808e26060d0f0b8bc936bc
Benjamin Liu <benjaminliu221@gmail.com>
2022-10-30 16:55:08 -0700

    fixed right arrow pagination

--- YelpScrape/YelpScrapeWeb/Models/YelpGraphQLBusinesses/SearchBusinessesResults.cs
+++ YelpScrape/YelpScrapeWeb/Models/YelpGraphQLBusinesses/SearchBusinessesResults.cs
@@ -5,6 +5,5 @@
         public List<Business> Businesses { get; set; }
         public SearchBusinessesArguments SearchBusinessesArguments { get; set; }
         public Dictionary<int, int> Paging { get; set; }
-        //public Dictionary<int, int> Paging = new Dictionary<int, int> { { 1, 0 }, { 2, 10 }, { 3, 20}, { 4, 30} };
     }
 }
---
+++ YelpScrape/YelpScrapeWeb/wwwroot/js/SearchBusinesses.js
@@ -0,0 +1,7 @@
+﻿<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+$(document).ready(function () {
+    $('#btnSubmit').click(function () {
+        $('.spinner').css('display', 'block');
+    });
+});
